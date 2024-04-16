3ad1049605cae792be90920b7f9cf29f1f8e0133
sdarek <darek.surdel@gmail.com>
2024-04-16 20:10:00 +0200

    base

---
+++ backend/src/test/java/com/example/sounds/SoundsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sounds;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoundsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
