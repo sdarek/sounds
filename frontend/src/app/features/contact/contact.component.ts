bf57ba7c78f6f5b53e09fdf9ebe057cfadebcc74
sdarek <darek.surdel@gmail.com>
2024-04-21 18:03:04 +0200

    contact + reservation

--- frontend/src/app/features/contact/contact.component.ts
+++ frontend/src/app/features/contact/contact.component.ts
@@ -1,12 +1,33 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { ReactiveFormsModule } from '@angular/forms'; 
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    ReactiveFormsModule
+  ],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss'
 })
 export class ContactComponent {
+  contactForm = new FormGroup({
+    name: new FormControl(''),
+    email: new FormControl(''),
+    message: new FormControl('')
+  });
+
+  onSubmit() {
+    console.log(this.contactForm.value);
+  }
 
 }
--- frontend/src/app/features/reservation/reservation.component.ts
+++ frontend/src/app/features/reservation/reservation.component.ts
@@ -1,12 +1,56 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormControl } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSelectModule } from '@angular/material/select';
+import { MatOptionModule } from '@angular/material/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+
+interface ReservationOption {
+  title: string;
+  duration: string;
+}
 
 @Component({
   selector: 'app-reservation',
-  standalone: true,
-  imports: [],
   templateUrl: './reservation.component.html',
-  styleUrl: './reservation.component.scss'
+  styleUrls: ['./reservation.component.scss'],
+  standalone: true,
+  imports: [
+    CommonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatSelectModule,
+    MatOptionModule,
+    MatCardModule,
+    MatDatepickerModule,
+    MatNativeDateModule
+  ]
 })
-export class ReservationComponent {
+export class ReservationComponent implements OnInit {
+  reservationItems: ReservationOption[] = [
+    { title: 'Nagranie muzyczne', duration: '1h' },
+    { title: 'Nagranie lektorskie', duration: '1h' },
+    { title: 'Sesja fotograficzna', duration: '2h' }
+  ];
+  
+  selectedReservationType?: ReservationOption;
+  selectedDate: Date | null = null;
 
+  ngOnInit(): void {}
+  onDateSelect(event: Date): void {
+    this.selectedDate = event;
+    console.log('Wybrano datę:', this.selectedDate);
+  }
+  
+
+  onReserve(): void {
+    console.log('Zarezerwowano:', this.selectedReservationType, 'na datę:', this.selectedDate);
+    // Implement further logic to handle the reservation
+  }
 }
